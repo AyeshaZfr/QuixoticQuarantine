72c5418d9e08e448e2e6ded69ff407c29ed12c81
Ayesha <ayeshamzfr@gmail.com>
2020-09-28 23:56:24 +0400

    updates

--- script.js
+++ script.js
@@ -1,70 +1,41 @@
+var boxIndex = 0;
+showBox();
 
-var figure = $(".video").hover( hoverVideo, hideVideo );
-
-function hoverVideo(e) {  
-    $('video', this).get(0).play(); 
+function showBox() {
+  var i;
+  var box = document.getElementsByClassName("box");
+  for (i = 0; i < box.length; i++) {
+    box[i].style.display = "none";
+  }
+  boxIndex++;
+  if (boxIndex > box.length - 3) {
+    boxIndex = 1
+  }
+  for (var i = 0; i < 4; i++) {
+    box[boxIndex + i - 1].style.display = "block";
+  }
+  setTimeout(showBox, 6000); // Change image every 3 seconds
 }
-
-function hideVideo(e) {
-    $('video', this).get(0).pause(); 
-}
-var slideIndex = 1;
-showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function navigate(n) {
+  showBoxManual(boxIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1; }
-    if (n < 1) { slideIndex = slides.length; }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function showBoxManual(n) {
+  var box = document.getElementsByClassName("box");
+  for (i = 0; i < box.length; i++) {
+    box[i].style.display = "none";
+  }
+  if (boxIndex < 0) {
+    boxIndex = box.length - 1;
+  }
+  for (var i = 0; i < 4; i++) {
+    var j = (boxIndex + i - 1) % 8;
+    if (j < 0) {
+      j = (box.length + j - 1);
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    box[j].style.display = "block";
+  }
 }
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml11 .line',
-    scaleY: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700
-  })
-  .add({
-    targets: '.ml11 .line',
-    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: 100
-  }).add({
-    targets: '.ml11 .letter',
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=775',
-    delay: (el, i) => 34 * (i+1)
-  }).add({
-    targets: '.ml11',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
